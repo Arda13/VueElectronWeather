e09b950034fb4746fe72b1070de60969088e5323
Arda Kaya <ardaka23@outlook.com>
2020-08-14 22:51:54 +0300

    first

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+import SearchBar from "./components/SearchBar";
+export default {
+  name: "electron-vue-weather",
+  components: { SearchBar },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
